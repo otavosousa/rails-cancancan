3fc00b51ddc30ac624973306f9903fb3743bdd01
Otavio Sousa <otavioklearskin@gmail.com>
2022-07-12 22:02:03 -0300

    config devise parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permitted_params, if: :devise_controller?
+
+  protected
+  def config_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation) }
+  end
 end
